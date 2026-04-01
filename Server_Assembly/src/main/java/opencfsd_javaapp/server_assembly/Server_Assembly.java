9a20e387fffce2f5081ebd863acf4724357054d9
WriteEnableQue, LaunchEnableQue, OpenAvrilConcurrency, OpenAvrilCFSD,  OpenAvrilNNI. <65234262+OpenAvril@users.noreply.github.com>
2026-04-01 19:57:13 +1000

    Add files via upload

---
+++ Server_Assembly/src/main/java/opencfsd_javaapp/server_assembly/Server_Assembly.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package opencfsd_javaapp.server_assembly;
+
+/**
+ *
+ * @author drago
+ */
+public class Server_Assembly {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
