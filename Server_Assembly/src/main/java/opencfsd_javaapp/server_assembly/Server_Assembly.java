004b29434a86096d7a6cac300f3c359813aea7d9
WriteEnableQue, LaunchEnableQue, OpenAvrilConcurrency, OpenAvrilCFSD,  OpenAvrilNNI. <65234262+OpenAvril@users.noreply.github.com>
2026-04-02 12:53:18 +1000

    Add files via upload

---
+++ Server_Assembly/src/main/java/opencfsd_javaapp/server_assembly/Server_Assembly.java
@@ -0,0 +1,36 @@
+package opencfsd_javaapp.server_assembly;
+
+import engine.Framework;
+
+public class Server_Assembly {
+    private static Framework pgmPtr_MathApp = null;
+    public static void main(String[] args) {
+        System.out.printf("entered first member function, main()%n");
+        pgmPtr_MathApp = main.IO.app_generate_Program();
+        System.out.printf("%n");
+        System.out.printf("        ,     \\      /      ,%n");
+        System.out.printf("       / \\    )\\ __ /(     / \\ %n");
+        System.out.printf("      /   \\   (_\\  /_)    /   \\ %n");
+        System.out.printf("____ / ____\\__ \\@  @/ ___/_____\\_____%n");
+        System.out.printf("|              |\\../|               |%n");
+        System.out.printf("|               \\VV/                |%n");
+        System.out.printf("|        OpenCFSD Java Assembly     |%n");
+        System.out.printf("|  - High Level Hardware Language.  |%n");
+        System.out.printf("|  - A Dynamic Link Library.      |%n");
+        System.out.printf("|___________________________________|%n");
+        System.out.printf("|    / \\ /        \\\\        \\ /\\    |%n");
+        System.out.printf("|  /    V          ))        V   \\  |%n");
+        System.out.printf("|/                //               \\|%n");
+        System.out.printf("`                 V                 '%n");
+        System.out.printf("%n");
+        System.out.printf("==========%n");
+        System.out.printf("decliration.%n");
+        System.out.printf("definition.%n");
+        System.out.printf("substanciation.%n");
+        System.out.printf("initialisation.%n");
+        System.out.printf("instanciation.%n");
+        System.out.printf("running.%n");
+        System.out.printf("==========%n");
+        System.out.printf("SIMULATION%n");
+    }
+}
